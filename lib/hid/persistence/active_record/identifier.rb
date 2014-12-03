2bb4c9df32de0d4dc1880be4f8f9db8e54aef257
Benjamin Cavileer <bcavileer@gmail.com>
2014-12-02 22:37:54 -0500

    Refactored the ActiveRecord persistence classes to be more flexible.

--- lib/hid/persistence/active_record/identifier.rb
+++ lib/hid/persistence/active_record/identifier.rb
@@ -2,19 +2,22 @@ module HID
   module Persistence
     module ActiveRecord
       class Identifier
+        class Identities < ::ActiveRecord::Base; end
+        @identities_class = Identities
+
         class << self
+          attr_accessor :identities_class
+
           def find(input, type)
-            identity = Identities.where(input: input, id_type: type).first
+            identity = identities_class.where(input: input, id_type: type).first
             identity && identity.identity
           end
 
           def map(input, identity, type)
-            Identities.create input: input, identity: identity, id_type: type
+            identities_class.create input: input, identity: identity, id_type: type
             nil
           end
         end
-
-        class Identities < ::ActiveRecord::Base; end
       end
     end
   end
--- lib/hid/persistence/active_record/recorder.rb
+++ lib/hid/persistence/active_record/recorder.rb
@@ -2,29 +2,38 @@ module HID
   module Persistence
     module ActiveRecord
       class Recorder
+        class Records < ::ActiveRecord::Base; end
+        @records_class = Records
+
         class << self
+          attr_accessor :records_class
+
           def record(input, type)
-            Records.create input: input, id_type: type
+            records_class.create input: input, id_type: type
             nil
           end
 
           def recorded(type)
             if type
-              Records.where(id_type: type).pluck(:input)
+              records_class.where(id_type: type).pluck(:input)
             else
-              record_groups = Records.all.group_by(&:id_type)
-              hash = Hash.new
-
-              record_groups.each do |type, records|
-                hash[type] = records.map(&:input) if records
-              end
-
-              hash
+              untyped_records
             end
           end
-        end
 
-        class Records < ::ActiveRecord::Base; end
+          private
+
+          def untyped_records
+            record_groups = records_class.all.group_by(&:id_type)
+            hash = Hash.new
+
+            record_groups.each do |type, records|
+              hash[type] = records.map(&:input) if records
+            end
+
+            hash
+          end
+        end
       end
     end
   end
